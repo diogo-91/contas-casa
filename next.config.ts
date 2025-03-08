1aa203e2291f322f7ec25fee1ae546310d659c68
Diogo <diogoespindolafranco@gmail.com>
2025-03-08 14:20:55 -0300

    Ajustes de configuração para deploy no Netlify

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  distDir: '.next',
+  trailingSlash: true
 };
 
-export default nextConfig;
+module.exports = nextConfig;
